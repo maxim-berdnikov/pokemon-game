02802b5ed908c0b6015cfdf0e6201275b393a8cf
Maxim Berdnikov <m.radik25@gmail.com>
2022-11-26 23:41:21 +0300

    update

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 const canvas = document.querySelector("canvas");
+const c = canvas.getContext("2d");
 
+canvas.width = 1024;
+canvas.height = 576;
+console.log(c);
 
+c.fillStyle = "#ffffff";
+c.fillRect(0, 0, canvas.width, canvas.height);
