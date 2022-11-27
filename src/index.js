99ec624db62d74ebdfb7c448d654c177ed57fff3
Maxim Berdnikov <m.radik25@gmail.com>
2022-11-27 22:38:54 +0300

    import map

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
-const canvas = document.querySelector("canvas");
-const c = canvas.getContext("2d");
+document.addEventListener("DOMContentLoaded", () => {
+  const canvas = document.querySelector("canvas");
+  const c = canvas.getContext("2d");
 
-canvas.width = 1024;
-canvas.height = 576;
-console.log(c);
+  canvas.width = 1024;
+  canvas.height = 576;
+  console.log(c);
 
-c.fillStyle = "#ffffff";
-c.fillRect(0, 0, canvas.width, canvas.height);
+  c.fillStyle = "#ffffff";
+  c.fillRect(0, 0, canvas.width, canvas.height);
+
+  const image = new Image();
+  image.src = "../assets/img/map.png";
+
+  image.onload = () => {
+    c.drawImage(image, -800, -450);
+  };
+});
